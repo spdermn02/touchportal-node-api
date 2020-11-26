003ab5e0c777eb6b16bcdf26d4a603cae7eed695
Jameson Allen (Spdermn02) <jameson.f.allen@gmail.com>
2020-11-25 21:01:20 -0600

    updates for prepping for 2.3 release fixing updateUrl checker fixing encoding of socket to be forced utf-8 fixing multi-actions coming through on single socket read which was causing issues

--- src/client.js
+++ src/client.js
@@ -171,12 +171,13 @@ class TouchPortalClient extends EventEmitter {
     let { pluginId, updateUrl } = options;
     this.pluginId = pluginId;
 
-    if ( updateUrl !== null ) {
-			this.updateUrl = updateUrl;
+    if ( updateUrl !== undefined ) {
+		  this.updateUrl = updateUrl;
 			this.checkForUpdate();
 		}
 
     this.socket = new net.Socket();
+    this.socket.setEncoding("utf-8");
     let that = this;
     this.socket.connect(SOCKET_PORT, SOCKET_IP, function () {
       console.log(that.pluginId, ": DEBUG : Connected to TouchPortal");
@@ -185,64 +186,78 @@ class TouchPortalClient extends EventEmitter {
     });
 
     this.socket.on("data", function (data) {
-      console.log("data received",data);
-      const message = JSON.parse(data.toString("utf-8"));
+      //console.log("data received - buffer ",data.toString('hex').match(/../g).join('\\x'));
+      //console.log("data received",data.toString("base64"));
+      //console.log("data received",data.toString("latin1"));
+      //console.log("data received",data.toString("utf-8"));
 
-      //Handle internal TP Messages here, else pass to user code
-      switch (message.type) {
-        case "closePlugin":
-          if (message.pluginId === that.pluginId) {
-            that.emit("Close", message);
+      let lines = data.split(/(?:\r\n|\r|\n)/);
+
+      lines.forEach( (line) => {
+        if( line == '' ) { return };
+        const message = JSON.parse(line);
+
+        //Handle internal TP Messages here, else pass to user code
+        switch (message.type) {
+          case "closePlugin":
+            if (message.pluginId === that.pluginId) {
+              that.emit("Close", message);
+              console.log(
+                that.pluginId,
+                ": WARN : received Close Plugin message"
+              );
+              that.socket.end();
+              process.exit(0);
+            }
+            break;
+          case "info":
+            console.log(that.pluginId, ": DEBUG : Info received");
+            that.emit("Info", message);
+            if( message.settings !== null ) {
+                that.emit("Settings", message.settings)
+            }
+            break;
+          case "settings":
+            console.log(that.pluginId, ": DEBUG : Settings Update received");
+            // values is the key that is the same as how info contains settings key, for direct settings saving
+            that.emit("Settings", message.values);
+            break;
+          case "listChange":
+            console.log(that.pluginId, ": DEBUG : ListChange received");
+            that.emit("ListChange", message);
+            break;
+          case "action":
+            console.log(that.pluginId, ": DEBUG : Action received");
+            that.emit("Action", message, null);
+            break;
+          case "broadcast":
+            console.log(that.pluginId, ": DEBUG : Broadcast Message received");
+            that.emit("Broadcast", message);
+            break;
+          case "up":
+            console.log(that.pluginId, ": DEBUG : Up Hold Message received");
+            that.emit("Action", message, false);
+            break;
+          case "down":
+            console.log(that.pluginId, ": DEBUG : Down Hold Message received");
+            that.emit("Action", message, true);
+            break;
+          default:
             console.log(
               that.pluginId,
-              ": WARN : received Close Plugin message"
+              `: DEBUG : Unhandled type received ${message.type}`
             );
-            that.socket.end();
-            process.exit(9);
-          }
-          break;
-        case "info":
-          console.log(that.pluginId, ": DEBUG : Info received");
-          that.emit("Info", message);
-          if( message.settings !== null ) {
-              that.emit("Settings", message.settings)
-          }
-          break;
-        case "settings":
-          console.log(that.pluginId, ": DEBUG : Settings Update received");
-          that.emit("Settings", message);
-          break;
-        case "listChange":
-          console.log(that.pluginId, ": DEBUG : ListChange received");
-          that.emit("ListChange", message);
-          break;
-        case "action":
-          console.log(that.pluginId, ": DEBUG : Action received");
-          that.emit("Action", message, null);
-          break;
-        case "broadcast":
-          console.log(that.pluginId, ": DEBUG : Broadcast Message received");
-          that.emit("Broadcast", message);
-          break;
-        case "up":
-          console.log(that.pluginId, ": DEBUG : Up Hold Message received");
-          that.emit("Action", message, false);
-          break;
-        case "down":
-          console.log(that.pluginId, ": DEBUG : Down Hold Message received");
-          that.emit("Action", message, true);
-          break;
-        default:
-          console.log(
-            that.pluginId,
-            `: DEBUG : Unhandled type received ${message.type}`
-          );
-          that.emit("Message", message);
-      }
+            that.emit("Message", message);
+        }
+      });
+    });
+    this.socket.on("error", function () {
+      console.log(that.pluginId, ": ERROR : Socket Connection closed");
+      process.exit(0);
     });
 
     this.socket.on("close", function () {
-      console.log(this.pluginId, ": WARN : Connection closed");
+      console.log(that.pluginId, ": WARN : Connection closed");
     });
   }
 }

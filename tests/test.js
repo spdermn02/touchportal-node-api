589d632d6f447cfd3309771f796013e2d522fd68
Andreas Schneider <andreas@razors.click>
2022-08-10 11:23:55 +0100

    Use eslint
    Installed eslint as a dev dependency (using the "AirBnb" style guide)
    Fixed most warnings flagged by eslint in src/
    Fixed warnings flagged by eslint in tests/
    Added "lint" script to package.json

--- tests/test.js
+++ tests/test.js
@@ -1,34 +1,31 @@
-"use strict";
+const TP = require('../src/index');
 
-const TP = require("../src/index.js");
-
-const pluginId = "TPExample";
+const pluginId = 'TPExample';
 
 const client = new TP.Client();
-client.on("Action", (data, isHeld) => {
-  console.log("Action:", JSON.stringify(data), "isHeld=" + isHeld);
+client.on('Action', (data, isHeld) => {
+  console.log('Action:', JSON.stringify(data), `isHeld=${isHeld}`);
 });
-client.on("ListChange", (data) => {
-  console.log("ListChange:", JSON.stringify(data));
+client.on('ListChange', (data) => {
+  console.log('ListChange:', JSON.stringify(data));
 });
-client.on("Message", (data) => {
-  console.log("Message:", JSON.stringify(data));
+client.on('Message', (data) => {
+  console.log('Message:', JSON.stringify(data));
 });
-client.on("Info", (data) => {
-  console.log("Info:", JSON.stringify(data));
+client.on('Info', (data) => {
+  console.log('Info:', JSON.stringify(data));
 });
-client.on("Settings",(data) => {
-  console.log("Info:", JSON.stringify(data));
+client.on('Settings', (data) => {
+  console.log('Info:', JSON.stringify(data));
 });
-client.on("ConnectorChange", (data) => {
-  console.log("Info:", JSON.stringify(data));
+client.on('ConnectorChange', (data) => {
+  console.log('Info:', JSON.stringify(data));
 });
-client.on("Broadcast",(data) => {
-  console.log("Info:", JSON.stringify(data));
-})
-client.on("Close", (data) => {
-  console.log("Closing Plugin:", JSON.stringify(data));
+client.on('Broadcast', (data) => {
+  console.log('Info:', JSON.stringify(data));
+});
+client.on('Close', (data) => {
+  console.log('Closing Plugin:', JSON.stringify(data));
 });
 
 client.connect({ pluginId });
-

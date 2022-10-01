16bd880e5b2650fd52204437f31fd18c749efae3
IMISSED <Damienweifen@gmail.com>
2022-09-30 17:12:48 -0700

    Fixed JSDoc

--- src/client.js
+++ src/client.js
@@ -19,17 +19,17 @@ class TouchPortalClient extends EventEmitter {
     this.customStates = {};
   }
 
+  /**
+   * Internal helper method for states
+   * 
+   * @param {Object[Object]} states - Array of object containing id,
+   * desc, defaultValue, optionally parentGroup.
+   * @param {String} stateType - TouchPortal state type removeState
+   * or createState.
+   * 
+   * @return {Array<Object>}
+   */
   stateHelper(states, stateType) {
-    /**
-     * Internal helper method for states
-     * 
-     * @param {Array<Object>} states - Array of object containing id,
-     * desc, defaultValue, optionally parentGroup.
-     * @param {String} stateType - TouchPortal state type removeState
-     * or createState.
-     * 
-     * @return {Array<Object>}
-     */
     const stateArray = [];
     if (!(typeof states == "object" || states.length)) {
       this.logIt('ERROR', `${stateType} has to be vaild object.`)
@@ -61,20 +61,21 @@ class TouchPortalClient extends EventEmitter {
     return stateArray;
   }
 
+  /**
+   * createState() allows you to create a state during runtime.
+   * 
+   * @param {String} id - unquie state id
+   * @param {String} desc - description of the state
+   * @param {String} defaultValue - default value for the state
+   * @param {String} parentGroup - Optionally parentGroup
+   * 
+   * @return {void}
+   */
   createState(id, desc, defaultValue, parentGroup) {
-    /**
-     * createState() allows you to create a state during runtime.
-     * 
-     * @param {String} id - unquie state id
-     * @param {String} desc - description of the state
-     * @param {String} defaultValue - default value for the state
-     * @param {String} parentGroup - Optionally parentGroup
-     * 
-     * @return {void}
-     */
     this.customStates[id] = desc;
     const stateArray = this.stateHelper(
       {
+        // @ts-ignore
         "id": id,
         "desc": desc,
         "defaultValue": defaultValue,
@@ -85,54 +86,55 @@ class TouchPortalClient extends EventEmitter {
     this.sendArray(stateArray)
   }
 
+  /**
+   * createStateMany() convenience function that
+   * allows you to create several states at once.
+   * 
+   * @param {Array<Object>} states - An array of object contains
+   * state id, state desc, defaultValue and optionally parentGroup.
+   * 
+   * @return {void}
+   */
   createStateMany(states) {
-    /**
-     * createStateMany() convenience function that
-     * allows you to create several states at once.
-     * 
-     * @param {Array<Object>} states - An array of object contains
-     * state id, state desc, defaultValue and optionally parentGroup.
-     * 
-     * @return {void}
-     */
     const stateArray = this.stateHelper(states, "createState")
     this.sendArray(stateArray)
   }
 
+  /**
+   * removeState() Allows you to remove certain states.
+   * 
+   * @param {String} id - state id state that you want to remove.
+   * 
+   * @return {void}
+   */
   removeState(id) {
-    /**
-     * removeState() Allows you to remove certain states.
-     * 
-     * @param {String} id - state id state that you want to remove.
-     * 
-     * @return {void}
-     */
+    // @ts-ignore
     const stateArray = this.stateHelper({"id": id}, "removeState")
     this.sendArray(stateArray)
   }
 
+  /**
+   * removeStateMany() convenience function to remove
+   * several states at once.
+   * 
+   * @param {Array<String>} states - A list of state id
+   * 
+   * @return {void}
+   */
   removeStateMany(states) {
-    /**
-     * removeStateMany() convenience function to remove
-     * several states at once.
-     * 
-     * @param {Array<String>} states - A list of state id
-     * 
-     * @return {void}
-     */
     const stateArray = this.stateHelper(states, "removeState")
     this.sendArray(stateArray)
   }
 
+  /**
+   * choiceUpdate() updates a choice data from a action.
+   * 
+   * @param {string} id - action choice data id
+   * @param {Array<String>} value - array of string
+   * 
+   * @return {void}
+   */
   choiceUpdate(id, value) {
-    /**
-     * choiceUpdate() updates a choice data from a action.
-     * 
-     * @param {string} id - action choice data id
-     * @param {Array<String>} value - array of string
-     * 
-     * @return {void}
-     */
     if (!(typeof value === "object")) {
       this.logIt('ERROR', 'choiceUpdate: value is an empty array');
       throw new Error('choiceUpdate: value is an empty array');
@@ -140,17 +142,17 @@ class TouchPortalClient extends EventEmitter {
     this.send({ type: 'choiceUpdate', id, value });
   }
 
+  /**
+   * choiceUpdateSpecific() allows you to update a specific 
+   * instances action data choices
+   * 
+   * @param {String} id - action choice data id
+   * @param {Array<String>} value - Array of strings.
+   * @param {string} instanceId - instanceId of the action
+   * 
+   * @return {void}
+   */
   choiceUpdateSpecific(id, value, instanceId) {
-    /**
-     * choiceUpdateSpecific() allows you to update a specific 
-     * instances action data choices
-     * 
-     * @param {String} id - action choice data id
-     * @param {Array<String>} value - Array of strings.
-     * @param {string} instanceId - instanceId of the action
-     * 
-     * @return {void}
-     */
     if (!value) {
       this.logIt('ERROR', 'choiceUpdateSpecific : value does not contain data in an array format');
       throw new Error(
@@ -169,15 +171,15 @@ class TouchPortalClient extends EventEmitter {
     });
   }
 
+  /**
+   * settingUpdate() allows you to update your plugin settings.
+   * 
+   * @param {String} name - setting name
+   * @param {String} value - new setting value
+   * 
+   * @return {void}
+   */
   settingUpdate(name, value) {
-    /**
-     * settingUpdate() allows you to update your plugin settings.
-     * 
-     * @param {String} name - setting name
-     * @param {String} value - new setting value
-     * 
-     * @return {void}
-     */
     this.send({
       type: 'settingUpdate',
       name,
@@ -185,29 +187,29 @@ class TouchPortalClient extends EventEmitter {
     });
   }
 
+  /**
+   * stateUpdate() allows you to update a states from the plugin either
+   * created in entry.tp or dynamically created.
+   * 
+   * @param {String} id - state id
+   * @param {String} value - a new value for the state.
+   * 
+   * @return {void}
+   */
   stateUpdate(id, value) {
-    /**
-     * stateUpdate() allows you to update a states from the plugin either
-     * created in entry.tp or dynamically created.
-     * 
-     * @param {String} id - state id
-     * @param {String} value - a new value for the state.
-     * 
-     * @return {void}
-     */
     this.send({ type: 'stateUpdate', id: `${id}`, value: `${value}` });
   }
 
+  /**
+   * stateUpdateMany() a method that allows you to update multiple
+   * states at once
+   * 
+   * @param {Array<object>} states - An array of object containing state id
+   * and state value
+   * 
+   * @return {void} 
+   */
   stateUpdateMany(states) {
-    /**
-     * stateUpdateMany() a method that allows you to update multiple
-     * states at once
-     * 
-     * @param {Array<object>} states - An array of object containing state id
-     * and state value
-     * 
-     * @return {void} 
-     */
     const stateArray = [];
 
     if (states.length <= 0) {
@@ -230,15 +232,16 @@ class TouchPortalClient extends EventEmitter {
     this.send(this.buildConnectorUpdate(id, value, data, isShortId));
   }
 
+  /**
+   * @param {String} id - connector id
+   * @param {number} value - connector value from 0 to 100.
+   * @param {Array<object>} data - An array of object containing
+   * id and value
+   * 
+   * @return {Object}
+   */
   buildConnectorUpdate(id, value, data, isShortId) {
-    /**
-     * @param {String} id - connector id
-     * @param {number} value - connector value from 0 to 100.
-     * @param {Array<object>} data - An array of object containing
-     * id and value
-     * 
-     * @return {Object}
-     */
+    // @ts-ignore
     const newValue = parseInt(value, 10);
     if (newValue < 0 || newValue > 100) {
       this.logIt('ERROR', `connectorUpdate: value has to be between 0 and 100 ${newValue}`);
@@ -265,13 +268,13 @@ class TouchPortalClient extends EventEmitter {
     return connectorUpdateObj;
   }
 
+  /**
+   * @param {Array<Object>} connectors - An array list of object containing shortId
+   * or normal connector id, value 0-100 and connector data.
+   * 
+   * @return {void}
+   */
   connectorUpdateMany(connectors) {
-    /**
-     * @param {Array<Object>} connectors - An array list of object containing shortId
-     * or normal connector id, value 0-100 and connector data.
-     * 
-     * @return {void}
-     */
     const connectorArray = [];
 
     if (connectors.length <= 0) {
@@ -286,16 +289,16 @@ class TouchPortalClient extends EventEmitter {
     this.sendArray(connectorArray);
   }
 
+  /**
+   * updateActionData() allows you dynamically update action data field
+   * minValue and maxValue
+   * 
+   * @param {String} actionInstanceId - action instance Id
+   * @param {Object} data - an object that contains minValue, maxValue, id and type
+   * 
+   * @return {void}
+   */
   updateActionData(actionInstanceId, data) {
-    /**
-     * updateActionData() allows you dynamically update action data field
-     * minValue and maxValue
-     * 
-     * @param {String} actionInstanceId - action instance Id
-     * @param {Object} data - an object that contains minValue, maxValue, id and type
-     * 
-     * @return {void}
-     */
     if (!data.id || !data.minValue || !data.maxValue || !data.type) {
       this.logIt('ERROR', 'updateActionData : required data is missing from instance', JSON.stringify(data));
       throw new Error(`updateActionData: required data is missing from instance. ${JSON.stringify(data)}`);
@@ -311,20 +314,20 @@ class TouchPortalClient extends EventEmitter {
     });
   }
 
+  /**
+   * sendNotification() this method allows your plugin to send
+   * notification to TouchPortal with a custom title, message
+   * actions buttons.
+   * 
+   * @param {String} notificationId - Unique ID of this notification
+   * @param {String} title - The notification title
+   * @param {String} msg - The message body text that is shown in the notification.
+   * @param {Array<Object>} optionsArray - List of options (actions) for the
+   * notification. Each options should be a object containing `id` and `title` keys.
+   * 
+   * @return {void}
+   */
   sendNotification(notificationId, title, msg, optionsArray) {
-    /**
-     * sendNotification() this method allows your plugin to send
-     * notification to TouchPortal with a custom title, message
-     * actions buttons.
-     * 
-     * @param {String} notificationId - Unique ID of this notification
-     * @param {String} title - The notification title
-     * @param {String} msg - The message body text that is shown in the notification.
-     * @param {Array<Object>} optionsArray - List of options (actions) for the
-     * notification. Each options should be a object containing `id` and `title` keys.
-     * 
-     * @return {void}
-     */
     if (optionsArray === undefined || optionsArray.length <= 0) {
       this.logIt('ERROR', 'sendNotification: at least one option is required');
       throw new Error('sendNotification: at least one option is required');
@@ -338,13 +341,13 @@ class TouchPortalClient extends EventEmitter {
     });
   }
 
+  /**
+   * sendArray() Internal method that combines multiple TouchPortal messages into one and send it.
+   * @param {Array<Object>} dataArray - An array of objects  
+   * 
+   * @return {void}
+   */
   sendArray(dataArray) {
-    /**
-     * sendArray() Internal method that combines multiple TouchPortal messages into one and send it.
-     * @param {Array<Object>} dataArray - An array of objects  
-     * 
-     * @return {void}
-     */
     let dataStr = '';
     if (dataArray.length <= 0) {
       this.logIt('ERROR', 'sendArray : dataArray has no length');
@@ -361,23 +364,23 @@ class TouchPortalClient extends EventEmitter {
     this.socket.write(dataStr);
   }
 
+  /**
+   * send() Normally you wouldn't need to use this method directly, but if this library
+   * does not cover something from TP api, this could be used instead.
+   * @param {Object} data - TouchPortal socket message
+   * 
+   * @return {void}
+   */
   send(data) {
-    /**
-     * send() Normally you wouldn't need to use this method directly, but if this library
-     * does not cover something from TP api, this could be used instead.
-     * @param {Object} data - TouchPortal socket message
-     * 
-     * @return {void}
-     */
     this.socket.write(`${JSON.stringify(data)}\n`);
   }
 
+  /**
+   * pair() Internal helper method that sends pair message to TouchPortal. 
+   * 
+   * @return {void}
+   */
   pair() {
-    /**
-     * pair() Internal helper method that sends pair message to TouchPortal. 
-     * 
-     * @return {void}
-     */
     const pairMsg = {
       type: 'pair',
       id: this.pluginId,
@@ -385,15 +388,15 @@ class TouchPortalClient extends EventEmitter {
     this.send(pairMsg);
   }
 
+  /**
+   * checkForUpdate() A method that http request
+   * to a Json that contains `version` key to compare.
+   * 
+   * @param {String} updateUrl - webpage url contains json
+   * 
+   * @return {void}
+   */
   checkForUpdate(updateUrl) {
-    /**
-     * checkForUpdate() A method that http request
-     * to a Json that contains `version` key to compare.
-     * 
-     * @param {String} updateUrl - webpage url contains json
-     * 
-     * @return {void}
-     */
     const parent = this;
     http.get(updateUrl, (res) => {
       const { statusCode } = res;
@@ -432,16 +435,16 @@ class TouchPortalClient extends EventEmitter {
     });
   }
 
+  /**
+   * connect() is main method that sets up TouchPortal connections 
+   * and callbacks. 
+   * 
+   * @param {String} pluginId - Plugin id that you've defined in entry.tp
+   * @param {String} updateUrl - Optional automatically checks for updates before starts connection.
+   * 
+   * @returns {void}
+   */
   connect(pluginId, updateUrl) {
-    /**
-     * connect() is main method that sets up TouchPortal connections 
-     * and callbacks. 
-     * 
-     * @param {String} pluginId - Plugin id that you've defined in entry.tp
-     * @param {String} updateUrl - Optional automatically checks for updates before starts connection.
-     * 
-     * @returns {void}
-     */
     this.pluginId = pluginId;
     const parent = this;
 
